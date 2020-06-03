7dc69efda4f92c50f0e53b2e0263b70b1fd61fbd
mortcotterell <66284832+mortcotterell@users.noreply.github.com>
2020-06-03 11:26:12 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,21 +1,25 @@
 class minecraft {
-  file {'/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+  $install_dir ='/opt/minecraft'
+file {$install_dir:
     ensure => directory,
     }
-  file   {'/opt/minecraft/server.jar':
+  file   {"${install_dir}/server.jar":
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
-   }
+    source => $url,
+    before => Service['minecraft'],
+  }
   package {'java':
-    ensure => present,
+    ensure  => present,
     }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt": 
     ensure => file,
     content => 'eula=true',
     }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
      }
   service {'minecraft':
     ensure => running,
