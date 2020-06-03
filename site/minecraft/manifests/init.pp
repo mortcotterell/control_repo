ed8f551169841ebf128ed9f28f0dbd24a056553b
mortcotterell <66284832+mortcotterell@users.noreply.github.com>
2020-06-03 11:49:46 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
   $install_dir = '/opt/minecraft'
   ){
-file {$install_dir
+file {$install_dir:
     ensure => directory,
     }
   file   {"${install_dir}/server.jar":
@@ -17,7 +17,7 @@ file {$install_dir
     ensure => file,
     content => 'eula=true',
     }
-  file {'/etc/systemd/system/minecraft.service':
+  file { '/etc/systemd/system/minecraft.service':
     ensure => file,
     content = epp('minecraft/minecraft.service', {
       install_dir => $install_dir,
