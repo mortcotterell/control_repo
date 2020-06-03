9ca5ceff12924617c5173034b67cd22eb3918ff0
mortcotterell <66284832+mortcotterell@users.noreply.github.com>
2020-06-03 08:29:58 +0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
                   ensure => present,
                   user   => 'root',
                   type   => 'ssh-rsa',
-                  key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCusOnYWbo2OUpKLnHtnuJYT5BB5kznym+y4nX+MgjvoLGa6PG6h3cQUEQcO9UkAV17AW+QcyiNo6VCFwxy6VBL6faJWcK0tMBlsPld1utlUI89AOqR3sIKz8aWgV18DSBl3UIX4Y7urvwPjaF97PDIySsYWdh6QS6loNnYSpSxATKTz4aByg1fh9Fk2+8BSs3pJSoZrWfwAFmmXoVg+jZjKffo1eionP2DQjX2AlFmXgPH4bzyOqIYOg8lVBew8qHoxVI9cw2TBBC6zpQqhWHoWMXJn2rePtsxYVCSspsVhiKjH4d4R+u3q+p20G+IdzgXTOEbwVpJocwWl0XMWUDT root@master.puppet.vm',
+                  key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCusOnYWbo2OUpKLnHtnuJYT5BB5kznym+y4nX+MgjvoLGa6PG6h3cQUEQcO9UkAV17AW+QcyiNo6VCFwxy6VBL6faJWcK0tMBlsPld1utlUI89AOqR3sIKz8aWgV18DSBl3UIX4Y7urvwPjaF97PDIySsYWdh6QS6loNnYSpSxATKTz4aByg1fh9Fk2+8BSs3pJSoZrWfwAFmmXoVg+jZjKffo1eionP2DQjX2AlFmXgPH4bzyOqIYOg8lVBew8qHoxVI9cw2TBBC6zpQqhWHoWMXJn2rePtsxYVCSspsVhiKjH4d4R+u3q+p20G+IdzgXTOEbwVpJocwWl0XMWUDT',
                   }
 }
