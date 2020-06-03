b06605b3c281167e5c13d16f3bba06f6af71fd39
mortcotterell <66284832+mortcotterell@users.noreply.github.com>
2020-06-03 09:59:49 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -16,7 +16,7 @@ class minecraft {
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft.service',
-     {
+     }
   service {'minecraft':
     ensure => running,
     enable => true,
