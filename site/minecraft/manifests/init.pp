179fa911cbbff8e8e98032fd82d8d20835e91af1
mortcotterell <66284832+mortcotterell@users.noreply.github.com>
2020-06-03 12:06:30 +0800

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,7 @@ file {$install_dir:
     }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    content = epp('minecraft/minecraft.service', {
+    content => epp('minecraft/minecraft.service', {
       install_dir => $install_dir,
     })
     }
